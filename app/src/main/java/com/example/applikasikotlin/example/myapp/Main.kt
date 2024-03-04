4185c642bf7f71a337b37a580544430adfb05458
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-04 17:37:13 +0700

    Latihan Created Class Codelab

---
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/Main.kt
@@ -0,0 +1,12 @@
+package com.example.applikasikotlin.example.myapp
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
+}
+
+fun main() {
+    buildAquarium()
+}
