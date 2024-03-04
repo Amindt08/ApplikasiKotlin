fd14e84178f357c8fcc9e1df863d97344d4ce881
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-04 10:02:37 +0700

    Latihan Membuat Class Object

--- app/src/main/java/com/example/applikasikotlin/Menu.kt
+++ app/src/main/java/com/example/applikasikotlin/Menu.kt
@@ -1,4 +1,22 @@
 package com.example.applikasikotlin
 
-class Menu {
+class Menu (var makanan: String, var minuman: String, var hargaMakanan: Int, var hargaMinuman: Int) {
+
+    fun updateMakanan (newMakanan: String){
+
+    }
+    fun updateMinuman (newMinuman: String){
+
+    }
+}
+
+class Pembeli (var nama: String, var jumlahPesanan: Int, var totalHarga: Int){
+}
+
+fun main(){
+    val myMenu = Menu ("Ayam Geprek", "Es Teh", 12000, 3000)
+    val myPembeli = Pembeli ("Amin", 2, 15000)
+    println("Pesanan atas nama ${myPembeli.nama} telah memesan item sebanyak ${myPembeli.jumlahPesanan} item")
+    println("yaitu ${myMenu.makanan} dengan harga Rp ${myMenu.hargaMakanan} dan ${myMenu.minuman} dengan harga Rp ${myMenu.hargaMinuman} ")
+    println("total harga pembelian 2 item Ayam Geprek dan Es Teh berjumlah Rp ${myPembeli.totalHarga}")
 }
