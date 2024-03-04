4b9c230511b92dfb865275f37389007aa34408ae
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-04 17:56:06 +0700

    Latihan Create a Class Contructions & Add Init Blocks Codelab

--- app/src/main/java/com/example/applikasikotlin/example/myapp/Aquarium.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/Aquarium.kt
@@ -1,10 +1,14 @@
 package com.example.applikasikotlin.example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
-    
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
