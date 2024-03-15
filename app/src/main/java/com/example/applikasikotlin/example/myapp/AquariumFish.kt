5645ba127b0055ed7d836fc24d68b850a16cb7ef
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-15 20:39:45 +0700

    Lesson 3 : Step 7- Make a new interface

--- app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
@@ -1,22 +1,22 @@
 package com.example.applikasikotlin.example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
-}
 interface FishAction {
     fun eat()
 }
-    class Shark: AquariumFish(), FishAction {
-        override val color = "grey"
-        override fun eat() {
-            println("hunt and eat fish")
-        }
+interface FishColor {
+    val color: String
+}
+class Plecostomus: FishAction, FishColor {
+    override val color = "gold"
+    override fun eat() {
+        println("eat algae")
     }
+}
+class Shark: FishAction, FishColor {
+    override val color = "grey"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
 
-    class Plecostomus: AquariumFish(), FishAction {
-        override val color = "gold"
-        override fun eat() {
-            println("eat algae")
-        }
-    }
 
