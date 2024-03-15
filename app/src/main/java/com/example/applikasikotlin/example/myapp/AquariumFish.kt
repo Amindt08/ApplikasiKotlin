c0d415176d10a6e453f7909cbc7bac5569422a5a
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-15 20:30:36 +0700

    Lesson 3 : Step 6 - Create an interface

--- app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
@@ -3,10 +3,20 @@ package com.example.applikasikotlin.example.myapp
 abstract class AquariumFish {
     abstract val color: String
 }
-class Shark: AquariumFish() {
-    override val color = "grey"
+interface FishAction {
+    fun eat()
 }
+    class Shark: AquariumFish(), FishAction {
+        override val color = "grey"
+        override fun eat() {
+            println("hunt and eat fish")
+        }
+    }
 
-class Plecostomus: AquariumFish() {
-    override val color = "gold"
-}
+    class Plecostomus: AquariumFish(), FishAction {
+        override val color = "gold"
+        override fun eat() {
+            println("eat algae")
+        }
+    }
+
--- app/src/main/java/com/example/applikasikotlin/example/myapp/Main.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/Main.kt
@@ -3,9 +3,10 @@ package com.example.applikasikotlin.example.myapp
 fun makeFish() {
     val shark = Shark()
     val pleco = Plecostomus()
-
     println("Shark: ${shark.color}")
+    shark.eat()
     println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
 
 fun main () {
