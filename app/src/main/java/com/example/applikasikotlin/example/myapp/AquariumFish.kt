e79bffdc4d9b95288370f7f690063b20e6b95e85
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-15 20:53:48 +0700

    Lesson 3 : Step 7 - Add interface delegation for FishColor

--- app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/AquariumFish.kt
@@ -6,17 +6,28 @@ interface FishAction {
 interface FishColor {
     val color: String
 }
-class Plecostomus: FishAction, FishColor {
+object GoldColor : FishColor {
     override val color = "gold"
+}
+class Plecostomus(fishColor: FishColor = GoldColor):  FishAction,
+    FishColor by fishColor {
     override fun eat() {
         println("eat algae")
     }
 }
+
+
 class Shark: FishAction, FishColor {
     override val color = "grey"
     override fun eat() {
         println("hunt and eat fish")
     }
 }
+class PrintingFishAction(val food: String) : FishAction {
+    override fun eat() {
+        println(food)
+    }
+}
+
 
 
