67cb7a080f6624b8185af220f95f122cd0e40b22
AMINDTMSTR\LENOVO <amindtmstr@gmail.com>
2024-03-18 08:19:17 +0700

    Lesson 3 : Step 9 - Create an enum

--- app/src/main/java/com/example/applikasikotlin/example/myapp/decor/Decoration.kt
+++ app/src/main/java/com/example/applikasikotlin/example/myapp/decor/Decoration.kt
@@ -14,9 +14,18 @@ fun makeDecorations() {
     println(wood)
     println(diver)
 }
-
-
-fun main(){
-    makeDecorations()
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
 }
+
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
+
+
+//fun main(){
+//    makeDecorations()
+//}
 
